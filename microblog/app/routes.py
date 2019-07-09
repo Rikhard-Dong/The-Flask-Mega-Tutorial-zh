e5936c783213ddd19faab6c540769e7d1ce7535a
donglinghao <supreDong@gmail.com>
2019-07-09 15:23:05 +0800

    chapter02 模板继承

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -6,4 +6,14 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {"username": "ride"}
-    return render_template("index.html", title="home", user=user)
+    posts = [
+        {
+            'author': {'username': '鲁迅'},
+            'body': '中国最伟大的艺术就是男人扮女人'
+        },
+        {
+            'author': {'username': '林则徐'},
+            'body': '苟利国家生死以, 岂因祸福避趋之!'
+        }
+    ]
+    return render_template("index.html", title="home", user=user, posts=posts)
