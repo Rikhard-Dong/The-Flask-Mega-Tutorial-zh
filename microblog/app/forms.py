cf986311351016196109ec385221b082cb7ce0b8
donglinghao <supreDong@gmail.com>
2019-07-10 13:56:32 +0800

    chapter05 用户注册与登陆

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('密码', validators=[DataRequired()])
     remember_me = BooleanField('记住我')
     submit = SubmitField('登陆')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('姓名', validators=[DataRequired()])
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    password2 = PasswordField('确认密码', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("注册")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('用户名已注册')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('邮箱已注册')
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,15 @@
-from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegisterForm
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 @app.route("/")
 @app.route('/index')
+@login_required
 def index():
     user = {"username": "ride"}
     posts = [
@@ -17,14 +22,50 @@ def index():
             'body': '苟利国家生死以, 岂因祸福避趋之!'
         }
     ]
-    return render_template("index.html", title="home", user=user, posts=posts)
+    return render_template("index.html", title="home", posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('登陆用户 {}, 记住我={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        username = form.username.data
+        password = form.password.data
+        remember_me = form.remember_me.data
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            flash("无效的用户名或者密码")
+            return redirect(url_for("login"))
+        login_user(user, remember=remember_me)
+        # 下一页
+        next_page = request.args.get('next')
+        if next_page is None or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='登陆', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        user = User(username=username, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        flash("恭喜! 注册成功!")
+        return redirect(url_for('login'))
+    return render_template('register.html', title='注册', form=form)
